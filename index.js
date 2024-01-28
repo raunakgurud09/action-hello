0fbf74edc85eaf920d5c95359d54c37ee8902638
Raunak Gurud <raunakgurud2121@gmail.com>
2024-01-28 17:30:10 +0530

    updated to new package

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import core from "@actions/core";
+import github from "@actions/github";
+import { publishToHashnode } from "./lib/publication.js";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const name = core.getInput("name");
-  const topic = core.getInput("topic");
-  const secret = core.getInput("secret");
+async function run() {
+  try {
+    const title = core.getInput("title");
+    const files = core.getInput("files");
+    const hashnode_key = core.getInput("hashnode_key");
 
-  console.log(`Hello ${name}!`);
-  const time = new Date().toTimeString();
+    core.setSecret(hashnode_key);
 
-  const message = `New blog published on topic ${topic} received your key ${secret}`;
-  console.log(`${message}`)
+    core.debug(
+      JSON.stringify({
+        title,
+        files,
+        hashnode_key,
+      })
+    );
 
-  core.setOutput("time", time);
-  core.setOutput("message", message);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // some function to analyze the post
+    // give output of the post
+    const results = await publishToHashnode({
+      title,
+      hashnode_key,
+      files,
+    });
 
-} catch (error) {
-  core.setFailed(error.message);
+    const output = results.map((r) => {
+      console.log(r);
+    });
+
+    const json = JSON.stringify(output, null, 2);
+    core.debug("Output result_json:\n" + json);
+    core.setOutput("result_json", json);
+
+    const summary = `output length: ${output.length}`;
+    core.setOutput("result_summary", summary);
+    
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
